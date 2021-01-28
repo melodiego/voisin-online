8d9fa31ba3d6ed1bd7257f680e981679eee9d6c2
Diego Melo <diego.melo@dbccompany.com.br>
2021-01-28 18:25:13 -0300

    First Commit Initial project

---
+++ src/main/java/br/com/voisinonline/VoisinOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.voisinonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoisinOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoisinOnlineApplication.class, args);
+    }
+
+}
